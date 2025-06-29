c8895352f5aaa62a44cbd57a60da9efef9896af5
deman404 <mr.aymanazhar.m@gmail.com>
2025-06-29 04:18:23 +0100

    refactor: remove unused analytics import from firebaseConfig

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
@@ -14,7 +13,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const database = getDatabase(app);
 
 export { app, database, firebaseConfig };
